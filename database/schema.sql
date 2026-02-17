d8d98464f202d7bb2ebc265dca82edbf0470fa57
xiaomi86pro <xiaomi86pro@gmail.com>
2026-02-17 15:01:12 +0000

    33

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,3 @@
--- WARNING: This schema is for context only and is not meant to be run.
--- Table order and constraints may not be valid for execution.
-
 CREATE TABLE public.exam_questions (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
   exam_id bigint NOT NULL,
