cb06d7489ffe69c3ef2912ede57bfc4dd4ff92ba
xiaomi86pro <xiaomi86pro@gmail.com>
2026-02-26 05:23:54 +0000

    1

--- lib/supabase/browser.ts
+++ lib/supabase/browser.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from "@supabase/ssr";
 
-export const supabaseBrowser = createClient(
+export const supabaseBrowser = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+);
